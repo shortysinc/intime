a2e26a0fb1440e6618ed3a959322e58fc3126370
shortysinc <jorge.shortys@gmail.com>
2015-02-04 18:38:29 +0100

    Update NetPort.java

--- NetPort/src/NetPort.java
+++ NetPort/src/NetPort.java
@@ -11,16 +11,14 @@
 
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    
+    @Author Jorge Rivas
+    @mail   jorge.shortys@gmail.com
  */
 
 import java.io.*;
 import java.net.*;
 
-
-/*
-    @Author Jorge Rivas
-    @mail   jorge.shortys@gmail.com
-*/
 /*
  * Este programa muestra las conexiones abiertas en un puerto sobre
  * un host determinado. Por defecto el host es localhost.
